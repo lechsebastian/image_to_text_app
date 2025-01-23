23c28710c8baa5e8e409693b622a49d95c1f7b39
Sebastian Lech <lechsebastian99@gmail.com>
2025-01-23 23:59:53 +0100

    Simplify app structure and set RecognitionScreen as home

---
+++ lib/pages/recognition_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RecognitionScreen extends StatefulWidget {
+  const RecognitionScreen({super.key});
+
+  @override
+  State<RecognitionScreen> createState() => _RecognitionScreenState();
+}
+
+class _RecognitionScreenState extends State<RecognitionScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
